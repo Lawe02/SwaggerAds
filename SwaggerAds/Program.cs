bbfd770527f95ae3c04cf9cf21f1eac4aaa6494a
Lawe Zangena <lawza02@gmail.com>
2023-06-16 01:30:19 +0200

    now then

--- SwaggerAds/Program.cs
+++ SwaggerAds/Program.cs
@@ -15,42 +15,43 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 
 // Added general description for Swagger
-builder.Services.AddSwaggerGen(sw =>
-{
-    // Needed for authorisation ========================================================
-    sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
-                      Enter 'Bearer' [space] and then your token in the text input below.
-                      \r\n\r\nExample: 'Bearer 12345abcdef'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    // Needed for authorisation ========================================================
-    sw.AddSecurityRequirement(new OpenApiSecurityRequirement()
-      {
-        {
-          new OpenApiSecurityScheme
-          {
-            Reference = new OpenApiReference
-              {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-              },
-              Scheme = "oauth2",
-              Name = "Bearer",
-              In = ParameterLocation.Header,
-            },
-            new List<string>()
-          }
-        });
-    // Needed for XML documentation to work
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    sw.IncludeXmlComments(xmlPath);
-});
+builder.Services.AddSwaggerGen();
+//sw =>
+//{
+//    // Needed for authorisation ========================================================
+//    sw.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+//    {
+//        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+//                      Enter 'Bearer' [space] and then your token in the text input below.
+//                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+//        Name = "Authorization",
+//        In = ParameterLocation.Header,
+//        Type = SecuritySchemeType.ApiKey,
+//        Scheme = "Bearer"
+//    });
+//    // Needed for authorisation ========================================================
+//    sw.AddSecurityRequirement(new OpenApiSecurityRequirement()
+//      {
+//        {
+//          new OpenApiSecurityScheme
+//          {
+//            Reference = new OpenApiReference
+//              {
+//                Type = ReferenceType.SecurityScheme,
+//                Id = "Bearer"
+//              },
+//              Scheme = "oauth2",
+//              Name = "Bearer",
+//              In = ParameterLocation.Header,
+//            },
+//            new List<string>()
+//          }
+//        });
+//    // Needed for XML documentation to work
+//    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+//    sw.IncludeXmlComments(xmlPath);
+//});
 
 builder.Services.ConfigureSwaggerGen(setup =>
 {
@@ -102,7 +103,7 @@ var app = builder.Build();
 //}
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
+    app.UseSwagger();
 if (app.Environment.IsDevelopment())
 {
 
